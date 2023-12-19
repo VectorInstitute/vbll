68f8816bbaa3b11651d0046aa312d6027ae3c1a0
jharrison42 <james.harrison@mail.mcgill.ca>
2023-12-19 15:13:18 -0800

    Added G-VBLL

--- vbll/layers/classification.py
+++ vbll/layers/classification.py
@@ -53,52 +53,50 @@ class VBLLClassificationD(nn.Module):
         self.W_mean = nn.Parameter(torch.randn(out_features, in_features))
         self.W_logdiag = nn.Parameter(torch.randn(out_features, in_features) - np.log(in_features))
         if parameterization == 'dense':
-          self.W_offdiag = nn.Parameter(torch.randn(out_features, in_features, in_features)/in_features)
+            self.W_offdiag = nn.Parameter(torch.randn(out_features, in_features, in_features)/in_features)
 
         if softmax_bound == 'jensen':
-          self.softmax_bound = self.jensen_bound
+            self.softmax_bound = self.jensen_bound
 
         self.return_ood = return_ood
 
     def noise_cov(self):
-      return torch.exp(self.noise_logdiag)
+        return torch.exp(self.noise_logdiag)
 
     def W_cov(self):
-      out = torch.exp(self.W_logdiag)
-      if self.W_dist == DenseNormal:
-        out = torch.tril(self.W_offdiag, diagonal=-1) + torch.diag_embed(out)
-      return out
+        out = torch.exp(self.W_logdiag)
+        if self.W_dist == DenseNormal:
+            out = torch.tril(self.W_offdiag, diagonal=-1) + torch.diag_embed(out)
+        return out
 
     def W(self):
-      return self.W_dist(self.W_mean, self.W_cov())
+        return self.W_dist(self.W_mean, self.W_cov())
 
     def noise(self):
-      return Normal(self.noise_mean, self.noise_cov())
+        return Normal(self.noise_mean, self.noise_cov())
 
     # ----- bounds
 
     def adaptive_bound(self, x, y):
-      # TODO(jamesharrison)
-      pass
+        # TODO(jamesharrison)
+        raise NotImplementedError('Adaptive bound not currently implemented')
 
     def jensen_bound(self, x, y):
-      pred = self.logit_predictive(x)
-
-      linear_term = pred.mean[torch.arange(x.shape[0]), y]
-      pre_lse_term = pred.mean + 0.5 * pred.covariance_diagonal
-      lse_term = torch.logsumexp(pre_lse_term, dim=-1)
-      return linear_term - lse_term
+        pred = self.logit_predictive(x)
+        linear_term = pred.mean[torch.arange(x.shape[0]), y]
+        pre_lse_term = pred.mean + 0.5 * pred.covariance_diagonal
+        lse_term = torch.logsumexp(pre_lse_term, dim=-1)
+        return linear_term - lse_term
 
     def montecarlo_bound(self, x, y, n_samples=10):
-      # TODO: fix this indexing
-      sampled_log_sm = F.log_softmax(self.logit_predictive(x).rsample(sample_shape=torch.Size([n_samples])), dim=-1)
-      mean_over_samples = torch.mean(sampled_log_sm, dim=0)
-      return mean_over_samples[torch.arange(x.shape[0]), y]
+        sampled_log_sm = F.log_softmax(self.logit_predictive(x).rsample(sample_shape=torch.Size([n_samples])), dim=-1)
+        mean_over_samples = torch.mean(sampled_log_sm, dim=0)
+        return mean_over_samples[torch.arange(x.shape[0]), y]
 
     # ----- forward and core ops
 
     def forward(self, x):
-        # todo add assert on shape of x input
+        # TODO(jamesharrison): add assert on shape of x input
         out = VBLLReturn(torch.distributions.Categorical(probs = self.predictive(x)),
                           self._get_train_loss_fn(x),
                           self._get_val_loss_fn(x))
@@ -109,7 +107,6 @@ class VBLLClassificationD(nn.Module):
         return (self.W() @ x[..., None]).squeeze(-1) + self.noise()
 
     def predictive(self, x, n_samples = 10):
-        # TODO: fix this indexing
         softmax_samples = F.softmax(self.logit_predictive(x).rsample(sample_shape=torch.Size([n_samples])), dim=-1)
         return torch.clip(torch.mean(softmax_samples, dim=0),min=0.,max=1.)
 
@@ -129,7 +126,127 @@ class VBLLClassificationD(nn.Module):
 
     def _get_val_loss_fn(self, x):
         def loss_fn(y):
-          return -torch.mean(torch.log(self.predictive(x)[torch.arange(x.shape[0]), y]))
+            return -torch.mean(torch.log(self.predictive(x)[torch.arange(x.shape[0]), y]))
+
+        return loss_fn
+
+    # ----- OOD metrics
+
+    def max_predictive(self, x):
+        return torch.max(self.predictive(x), dim=-1)[0]
+
+class VBLLClassificationG(nn.Module):
+    # TODO(jamesharrison): add dirichlet
+    def __init__(self,
+                 in_features,
+                 out_features,
+                 regularization_weight,
+                 parameterization='dense',
+                 softmax_bound='jensen',
+                 return_ood=False,
+                 prior_scale=1.,
+                 wishart_scale=1.,
+                 dof=1.):
+        super(VBLLClassificationG, self).__init__()
+
+        self.wishart_scale = wishart_scale
+        self.dof = (dof + in_features + 1.)/2.
+        self.regularization_weight = regularization_weight
+
+        # define prior, currently fixing zero mean and arbitrarily scaled cov
+        self.prior_scale = prior_scale
+
+        # noise distribution
+        self.noise_mean = nn.Parameter(torch.zeros(in_features), requires_grad = False)
+        self.noise_logdiag = nn.Parameter(torch.randn(in_features))
+
+        # last layer distribution
+        self.mu_dist = get_parameterization(parameterization)
+        self.mu_mean = nn.Parameter(0.1*torch.randn(out_features, in_features))
+        self.mu_logdiag = nn.Parameter(torch.randn(out_features, in_features))
+        if parameterization == 'dense':
+            raise NotImplementedError('Dense embedding cov not implemented for g-vbll')
+
+        if softmax_bound == 'jensen':
+            self.softmax_bound = self.jensen_bound
+
+        self.return_ood = return_ood
+
+    def noise_cov(self):
+        return torch.exp(self.noise_logdiag)
+
+    def mu_cov(self):
+        out = torch.exp(self.mu_logdiag)
+        # TODO(jamesharrison): add impl for dense/low rank cov
+        return out
+
+    def mu(self):
+        return self.mu_dist(self.mu_mean, self.mu_cov())
+
+    def noise(self):
+        return Normal(self.noise_mean, self.noise_cov())
+
+    # ----- bounds
+
+    def adaptive_bound(self, x, y):
+        # TODO(jamesharrison)
+        raise NotImplementedError('Adaptive bound not implemented for g-vbll')
+
+    def jensen_bound(self, x, y):
+        linear_pred = self.noise() + self.mu_mean[y]
+        linear_term = linear_pred.log_prob(x)
+        if isinstance(linear_pred, Normal):
+            # Is there a more elegant way to handle this?
+            linear_term = linear_term.sum(-1)
+
+        trace_term = (self.mu_cov()[y] / self.noise_cov()).sum(-1)
+
+        pre_lse_term = self.logit_predictive(x)
+        lse_term = torch.logsumexp(pre_lse_term, dim=-1)
+        return linear_term - 0.5 * trace_term - lse_term
+
+    def montecarlo_bound(self, x, y, n_samples=10):
+        # TODO(jamesharrison)
+        raise NotImplementedError('Monte carlo bound not implemented for g-vbll')
+
+    # ----- forward and core ops
+
+    def forward(self, x):
+        # TODO(jamesharrison): add assert on shape of x input
+        out = VBLLReturn(torch.distributions.Categorical(probs = self.predictive(x)),
+                          self._get_train_loss_fn(x),
+                          self._get_val_loss_fn(x))
+        if self.return_ood: out.ood_scores = self.max_predictive(x)
+        return out
+
+    def logit_predictive(self, x):
+        # likelihood of x under marginalized
+        logprob = (self.mu() + self.noise()).log_prob(x.unsqueeze(-2))
+        if isinstance(self.mu(), Normal):
+            # Is there a more elegant way to handle this?
+            logprob = logprob.sum(-1)
+        return logprob
+
+    def predictive(self, x):
+        return torch.clip(F.softmax(self.logit_predictive(x), dim=-1), min=0., max=1.)
+
+    def _get_train_loss_fn(self, x):
+
+        def loss_fn(y):
+            # print('computing loss fn')
+            noise = self.noise()
+            kl_term = KL(self.mu(), self.prior_scale)
+            wishart_term = (self.dof * noise.logdet_precision - 0.5 * self.wishart_scale * noise.trace_precision)
+
+            total_elbo = torch.mean(self.softmax_bound(x, y))
+            total_elbo += self.regularization_weight * (wishart_term - kl_term)
+            return -total_elbo
+
+        return loss_fn
+
+    def _get_val_loss_fn(self, x):
+        def loss_fn(y):
+            return -torch.mean(torch.log(self.predictive(x)[np.arange(x.shape[0]), y]))
 
         return loss_fn
 
