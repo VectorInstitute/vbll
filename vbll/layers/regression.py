1efa3732f6b24e31002cf2e087eca880e464cbc8
jharrison42 <james.harrison@mail.mcgill.ca>
2024-01-09 10:14:35 -0800

    Update regression.py
    added missing import

--- vbll/layers/regression.py
+++ vbll/layers/regression.py
@@ -1,7 +1,7 @@
 import torch
 from dataclasses import dataclass
 from vbll.vbll.utils.distributions import Normal, DenseNormal
-
+from collections.abc import Callable
 
 def KL(p, q_scale):
     feat_dim = p.mean.shape[-1]
