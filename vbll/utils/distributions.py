b7417358534207d29bcb513c83fb31a22ac1d374
jharrison42 <james.harrison@mail.mcgill.ca>
2024-01-11 10:37:29 -0800

    Update distributions.py
    bug fixes in covariances

--- vbll/utils/distributions.py
+++ vbll/utils/distributions.py
@@ -21,56 +21,64 @@ def sym(M):
 
 class Normal(torch.distributions.Normal):
     def __init__(self, loc, var):
-        super(Normal, self).__init__(loc, var)
+        super(Normal, self).__init__(loc, torch.sqrt(torch.clip(var, min = 1e-6)))
+
+    @property
+    def mean(self):
+        return self.loc
+
+    @property
+    def var(self):
+        return self.scale ** 2
 
     @property
     def chol_covariance(self):
-        return torch.diag_embed(self.scale.sqrt())
-
-    @property
-    def covariance_diagonal(self):
-        return self.scale
-
-    @property
-    def covariance(self):
         return torch.diag_embed(self.scale)
 
     @property
+    def covariance_diagonal(self):
+        return self.var
+
+    @property
+    def covariance(self):
+        return torch.diag_embed(self.var)
+
+    @property
     def precision(self):
-        return torch.diag_embed(1./self.scale)
+        return torch.diag_embed(1./self.var)
 
     @property
     def logdet_covariance(self):
-        return torch.log(self.scale).sum(-1)
+        return 2 * torch.log(self.scale).sum(-1)
 
     @property
     def logdet_precision(self):
-        return -torch.log(self.scale).sum(-1)
+        return -2 * torch.log(self.scale).sum(-1)
 
     @property
     def trace_covariance(self):
-        return self.scale.sum(-1)
+        return self.var.sum(-1)
 
     @property
     def trace_precision(self):
-        return 1./self.scale.sum(-1)
+        return (1./self.var).sum(-1)
 
     def covariance_weighted_inner_prod(self, b, reduce_dim=True):
         assert b.shape[-1] == 1
-        prod = (self.scale.unsqueeze(-1) * (b ** 2)).sum(-2)
+        prod = (self.var.unsqueeze(-1) * (b ** 2)).sum(-2)
         return prod.squeeze(-1) if reduce_dim else prod
 
     def precision_weighted_inner_prod(self, b, reduce_dim=True):
         assert b.shape[-1] == 1
-        prod = (1./self.scale.unsqueeze(-1) * (b ** 2)).sum(-2)
+        prod = ((b ** 2)/self.var.unsqueeze(-1)).sum(-2)
         return prod.squeeze(-1) if reduce_dim else prod
 
     def __add__(self, inp):
         if isinstance(inp, Normal):
-            new_cov =  self.scale + inp.scale
-            return Normal(self.loc + inp.loc, torch.clip(new_cov, min = 1e-8))
+            new_cov =  self.var + inp.var
+            return Normal(self.mean + inp.mean, torch.clip(new_cov, min = 1e-12))
         elif isinstance(inp, torch.Tensor):
-            return Normal(self.loc + inp, self.scale)
+            return Normal(self.mean + inp, self.var)
         else:
             raise NotImplementedError('Distribution addition only implemented for diag covs')
 
@@ -78,11 +86,10 @@ class Normal(torch.distributions.Normal):
         assert inp.shape[-2] == self.loc.shape[-1]
         assert inp.shape[-1] == 1
         new_cov = self.covariance_weighted_inner_prod(inp.unsqueeze(-3), reduce_dim = False)
-        return Normal(self.loc @ inp, torch.clip(new_cov, min = 1e-8))
+        return Normal(self.loc @ inp, torch.clip(new_cov, min = 1e-12))
 
     def squeeze(self, idx):
-        return Normal(self.loc.squeeze(idx), self.scale.squeeze(idx))
-
+        return Normal(self.loc.squeeze(idx), self.scale.squeeze(idx) ** 2)
 
 class DenseNormal(torch.distributions.MultivariateNormal):
     def __init__(self, loc, cholesky):
@@ -107,7 +114,7 @@ class DenseNormal(torch.distributions.MultivariateNormal):
 
     @property
     def logdet_covariance(self):
-        return torch.diagonal(self.scale_tril, dim1=-2, dim2=-1).log().sum(-1)
+        return 2. * torch.diagonal(self.scale_tril, dim1=-2, dim2=-1).log().sum(-1)
 
     @property
     def trace_covariance(self):
@@ -127,7 +134,7 @@ class DenseNormal(torch.distributions.MultivariateNormal):
         assert inp.shape[-2] == self.loc.shape[-1]
         assert inp.shape[-1] == 1
         new_cov = self.covariance_weighted_inner_prod(inp.unsqueeze(-3), reduce_dim = False)
-        return Normal(self.loc @ inp, torch.clip(new_cov, min = 1e-8))
+        return Normal(self.loc @ inp, torch.clip(new_cov, min = 1e-12))
 
     def squeeze(self, idx):
         return DenseNormal(self.loc.squeeze(idx), self.scale_tril.squeeze(idx))
